d2039b5b6e3518405d8ec3c1c5d7bb2538d64138
Sungmin Cho <sc107@illinois.edu>
2022-10-19 02:17:15 -0500

    Intialized Project M

---
+++ game/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "prog.h"
+
+
+/* 
+* This is the main code for the Project M.
+* It calls the prog.c, which contians sets of functions. 
+*/
+
+int main(){
+    title_page();
+    return 0;
+}
---
+++ game/prog.c
@@ -0,0 +1,30 @@
+// PROJECT M (Beta Ver.) by Sungmin Cho & Jeremy Lau
+// 10. 19. 2022
+
+/*
+* Project M, or "Man On!" for the release title, is a text-based soccer
+* management game, where the user is able to play as the manager of a soccer team.
+* Some of the main features of beta version of the game include:
+*   - Fully Customizable Team
+*   - Simulated Matches
+*   - Player Auction Market
+*   - Future Event Calendar
+*
+* To be considered as Version 1.0 of the project, we aim to include 
+* the following extended features:
+*   - Random Events (weather, injuries)
+*   - More Teams, Leagues and Players
+*   - More Tactics
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "prog.h"
+
+
+// Prints title page
+void title_page(){
+    printf("Man On!\n");
+    printf("Press any button to start");
+}
+

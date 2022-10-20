fa8471a0b03eed7be0dc773383e65b49eecbad51
Sungmin Cho <sc107@illinois.edu>
2022-10-20 14:42:10 -0500

    Implemented Quit

--- main.c
+++ main.c
@@ -8,9 +8,17 @@
 */
 
 int main(){
+    char action;
+    //int asset = 0;          // player starting money
+
     title_page();           // title page
 
     main_page();            // main menu    
+    printf("What would you like to do? ");
+    scanf("%c", &action);
+
+    // Quits
+    if (action == 'q' || action == 'Q') return 0;      
 
 
     return 0;
